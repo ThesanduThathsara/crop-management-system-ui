7e277504c47ea0575a1262ed9a18be742477e048
Thesandu Thathsara <thesandu1602500@gmail.com>
2025-01-20 14:04:02 +0530

    add meta data

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import {useState} from 'react'
 import './App.css'
 import Login from "./pages/Login.tsx";
 import SignUp from "./pages/SignUp.tsx";
@@ -7,17 +7,25 @@ import Crop from "./pages/Crop.tsx";
 import Staff from "./pages/Staff.tsx";
 import Log from "./pages/Log.tsx";
 import Vehicle from "./pages/Vehicle.tsx";
+import {Helmet} from "react-helmet";
 import Equipment from "./pages/Equipment.tsx";
 
 
 function App() {
-  const [count, setCount] = useState(0)
+    const [count, setCount] = useState(0)
 
-  return (
-    <>
-        <Equipment/>
-    </>
-  )
+    return (
+        <>
+            <Helmet>
+                <meta name="description" content="Crop monitoring system "/>
+                <meta name="keywords" content="Green Shadow (Pvt) Ltd "/>
+                <meta name="author" content="Thesandu Thathsara"/>
+                <title>Crop Monitoring System </title>
+                <link rel="icon" type="image/x-icon" href="assets/green-shadow-high-resolution-logo-transparent.png"/>
+            </Helmet>
+            <Equipment/>
+        </>
+    )
 }
 
 export default App
